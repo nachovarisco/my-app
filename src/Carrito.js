93b9566e7824f56f6980110af373e1c22d5a18f8
IgnacioVarisco <varisconacho@gmail.com>
2022-11-17 08:31:16 -0300

    formulario atr

--- src/Carrito.js
+++ src/Carrito.js
@@ -1,14 +1,47 @@
-import React from 'react'
-import ContadorContainer from './ContadorContainer'
+import React ,{useState} from 'react'
+
 
 
 
 
 
 const Carrito = () => {
+
+
+  const [valores, setValores] = useState ({
+    name:"",
+    edad :""
+  })
+   
+  
+
+const handleChange = (e) => {
+ setValores  ({
+
+  ...valores,         
+  [e.target.dataset.target] : e.target.value
+ })
+
+
+}
+
+  const handleSubmit = (e) => {
+  e.preventDefault()
+  console.log(valores)
+  
+}
+
   return (
     <div>
-    
+   <form onSubmit={handleSubmit}>
+    <div className='container'>
+      <input data-target =  "name"  type= "text" onChange={handleChange}/>
+    </div>
+    <div>
+      <input data-target = "edad" type= "text" onChange={handleChange}/>
+    </div>
+    <button> guardar    </button>
+   </form>
     </div>
   )
 }
