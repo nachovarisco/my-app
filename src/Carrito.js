550aeed579c6d6452d2b0f8652190f5688c363f2
IgnacioVarisco <varisconacho@gmail.com>
2022-12-08 18:48:35 -0300

    productos en consola de carrito a iterar

--- src/Carrito.js
+++ src/Carrito.js
@@ -1,66 +1,25 @@
-import { collection, addDoc , serverTimestamp } from 'firebase/firestore'
-import React ,{useRef} from 'react'
-import { useCarrito } from './CartProvider'
-import { db } from './firebase'
-
-
-
-
-
-
-
+import React from 'react'
 
 const Carrito = () => {
 
-const valorDelContexto = useCarrito()
- 
-  
-const refName = useRef()
-const refTelefono = useRef()
+
+
+const storageProducts = localStorage.getItem("producto")
+console.log(storageProducts)
 
 
 
-  const handleSubmit = (e) => {
-  e.preventDefault()
-
-   const orden = {
-    buyer : {
-      name: refName,
-    
-      phone : refTelefono,
 
 
-    },
-    products : [],
-    total: 100,
-    date : serverTimestamp(),
-   }
-   console.log(refName,refTelefono)
-  
-  const ordersCollection = collection(db, "usuarios")
-  const consulta = addDoc(ordersCollection, orden)
 
-  consulta 
-  .then((docRef) =>{
-    console.log(docRef.id)
 
-  })
-  .then((error)=>
-  console.log(error))
-  
-}
+
+
 
   return (
+
     <div>
-   <form onSubmit={handleSubmit}>
-    <div className='container'>
-      <input ref={ refName} type= "text"/>
-    </div>
-    <div>
-      <input  ref={refTelefono} type= "text"/>
-    </div>
-    <button> guardar    </button>
-   </form>
+    <h2> aca van los productos iterados que estan en la consola</h2>
     </div>
   )
 }
